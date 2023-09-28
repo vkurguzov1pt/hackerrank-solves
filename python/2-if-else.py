0018a28b26f21dd2434933e419f8215fb3310196
Victor Kurguzov <victor.kurguzov@carrier.com>
2023-09-28 23:46:45 +0200

    upd: pylint fixes

--- python/2-if-else.py
+++ python/2-if-else.py
@@ -10,12 +10,6 @@ If n is even and in the inclusive range of 6 to 20 , print Weird
 If n is even and greater than 20, print Not Weird
 """
 
-import math
-import os
-import random
-import re
-import sys
-
 #################
 # My Solution
 #################
@@ -24,15 +18,14 @@ def what_is_n(n):
     message = ''
     if n % 2 != 0:
         message = "Weird"
-    elif n % 2 == 0 and n in range(2,6):
+    elif n % 2 == 0 and n in range(2, 6):
         message = "Not Weird"
-    elif n % 2 == 0 and n in range(6,21):
+    elif n % 2 == 0 and n in range(6, 21):
         message = "Weird"
     elif n % 2 == 0 and n > 20:
         message = "Not Weird"
 
-    return message
-      
+    return message    
 
 if __name__ == '__main__':
     n = int(input().strip())

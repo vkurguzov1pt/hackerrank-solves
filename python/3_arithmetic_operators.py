e610379a4b7b752b02b3565fa3b110a0a1446014
Victor Kurguzov <victor.kurguzov@carrier.com>
2023-09-29 13:50:10 +0200

    [Python] 3rd problem solution

---
+++ python/3_arithmetic_operators.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Task
+The provided code stub reads two integers from STDIN, a and b.
+Add code to print three lines where:
+
+  The first line contains the sum of the two numbers.
+  The second line contains the difference of the two numbers (first - second).
+  The third line contains the product of the two numbers.
+"""
+
+def print_operations(a,b):
+  print(a+b,a-b,a*b, sep='\n')
+    
+if __name__ == '__main__':
+  a = int(input())
+  b = int(input())
+  print_operations(a,b)

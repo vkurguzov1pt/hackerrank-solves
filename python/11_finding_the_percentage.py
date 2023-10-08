a70cd1048bad1d180754a479f5b7c0057d5916c9
Victor Kurguzov <vic.kurguzov@gmail.com>
2023-10-08 23:30:20 +0200

    [Python] 11th Problem Solution

--- python/11_finding_the_percentage.py
+++ python/11_finding_the_percentage.py
@@ -6,11 +6,22 @@
   showing 2 places after the decimal. 
 """
 
-def func():
+def func(data: dict, query: str) -> float:
     """
-    See the task #2
+    get the average mark for specified name aka 'query'
     """
+    marks = sum(data[query])/len(data[query])
+
+    return marks
 
 
 if __name__ == '__main__':
-    print(func())
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    result_mark = func(student_marks, query_name)
+    print(f"{result_mark:.2f}")

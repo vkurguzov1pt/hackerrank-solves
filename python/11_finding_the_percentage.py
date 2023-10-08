40eb9ea807f1f101e2dbf4027293616e08033ead
Victor Kurguzov <victor.kurguzov@carrier.com>
2023-10-08 02:27:03 +0200

    [Python] 11th Problem init

--- python/11_finding_the_percentage.py
+++ python/11_finding_the_percentage.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 """
+  The provided code stub will read in a dictionary containing key/value pairs
+  of name:[marks] for a list of students. 
+  Print the average of the marks array for the student name provided, 
+  showing 2 places after the decimal. 
 """
 
 def func():
-  pass
+    """
+    See the task
+    """
+
+
+if __name__ == '__main__':
+    print(func())
